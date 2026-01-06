bf03f1fb6a01890e4793a32d77e5a7255020e440
prasenjitovhal <133623314+prasenjitovhal@users.noreply.github.com>
2026-01-07 00:41:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,13 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-kernel-*"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-
   filter {
-    name   = "architecture"
-    values = ["x86_64"]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
-
 resource "aws_instance" "jenkins_demo" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"  # Free tier eligible
@@ -24,4 +22,5 @@ resource "aws_instance" "jenkins_demo" {
 
 output "instance_public_ip" {
   value = aws_instance.jenkins_demo.public_ip
+
 }
